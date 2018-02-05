80cc48ac14d900ecb6a44744f0d1f7353fde6aad
Jeffrey Lam <jeffrey.clam95@gmail.com>
2018-02-05 15:35:10 -0800

    Starting the project

---
+++ script.js
@@ -0,0 +1,9 @@
+var buttons = document.getElementsByName("options");
+
+function println(text) {
+    var ln = "<br>";
+    document.write(text + ln);
+}
+
+println("hello");
+println(typeof buttons);
