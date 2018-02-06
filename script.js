f444935f414f7f413d472e56e3281a82d102bb78
Jeffrey Lam <jeffrey.clam95@gmail.com>
2018-02-06 10:34:53 -0800

    Interactive buttons Rock, Paper, Scissors

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
-var buttons = document.getElementsByName("options");
-
 function println(text) {
     var ln = "<br>";
     document.write(text + ln);
 }
 
-println("hello");
-println(typeof buttons);
+var outcome = document.createElement("p");
+var outcomeText = document.createTextNode("Result");
+outcome.appendChild(outcomeText);
+document.body.appendChild(outcome);
+
+function setOutcome(result) {
+    outcomeText.nodeValue = result;
+}
+
+var options = ["Rock", "Paper", "Scissors"];
+var buttons = [];
+
+for (var i = 0; i < options.length; i++) {
+    buttons[i] = document.createElement("button");
+    var textNode = document.createTextNode(options[i]);
+    buttons[i].appendChild(textNode);
+    document.body.appendChild(buttons[i]);
+    buttons[i].addEventListener("click", 
+        setOutcome.bind(null, options[i]));
+}
+
+println("");
