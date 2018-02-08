42b894ecd8b33d6aa967ff0abc00fd4ae3a43e58
Jeffrey Lam <jeffrey.clam95@gmail.com>
2018-02-07 23:55:54 -0800

    Fixing game logic and adding personalized "play again" message

--- script.js
+++ script.js
@@ -5,28 +5,44 @@ function println(text) {
 
 var options = ["Rock", "Paper", "Scissors"];
 
-function setOutcome(option) {
+function setOutcome(players_choice) {
     var result = document.getElementById("players_choice");
-    result.innerHTML = option;
-    play(option);
+    result.innerHTML = players_choice;
+    play(players_choice);
 }
 
-function play(option) {
+function play(players_choice) {
     var result = document.getElementById("computers_choice");
     var random = Math.floor(Math.random() * 3);
-    var choice = options[random];
-    switch(choice) {
-        case 'Rock':
-            result.innerHTML = 'Paper';
-            break;
-        case 'Paper':
-            result.innerHTML = 'Scissors';
-            break;
-        case 'Scissors':
-            result.innerHTML = 'Rock';
-            break;
-        default:
-            result.innerHTML = choice + ': How did you even get here?';
+    var computers_choice = options[random];
+    result.innerHTML = options[random];
+    calculateResults(players_choice, computers_choice);
+}
+
+function calculateResults(players_choice, computers_choice) {
+    var intro = document.getElementById("intro");
+    if (players_choice === computers_choice) {
+        intro.innerHTML = 'It\'s a draw... Let\'s play again!';
+    } else {
+        if (players_choice === "Rock") {
+            if (computers_choice === "Paper") {
+                intro.innerHTML = 'I win! Let\'s play again!';
+            } else {
+                intro.innerHTML = 'I lost... Let me beat you!';
+            }
+        } else if (players_choice === "Paper") {
+            if (computers_choice === "Scissors") {
+                intro.innerHTML = 'I win! Let\'s play again!';
+            } else {
+                intro.innerHTML = 'I lost... Let me beat you!';
+            }
+        } else if (players_choice === "Scissors") {
+            if (computers_choice === "Rock") {
+                intro.innerHTML = 'I win! Let\'s play again!';
+            } else {
+                intro.innerHTML = 'I lost... Let me beat you!';
+            }
+        }
     }
 }
 
@@ -37,5 +53,3 @@ var buttons = [document.getElementById("rock"),
 buttons[0].addEventListener('click', setOutcome.bind(null, options[0]));
 buttons[1].addEventListener('click', setOutcome.bind(null, options[1]));
 buttons[2].addEventListener('click', setOutcome.bind(null, options[2]));
-
-println("");
